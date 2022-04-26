547fbf782870bde1bd16fb7635efa25208fa6d05
SurajK2025 <suraj.khokrale@rediffmail.com>
2022-04-26 18:26:27 +0530

    Final Commit

---
+++ Hibernate-Mappings/src/main/java/com/oneToMany/Answers.java
@@ -0,0 +1,28 @@
+package com.oneToMany;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Answers {
+	
+	@Id
+	@Column(name = "ansId")
+	private int answerId;
+	private String answer;
+	public int getAnswerId() {
+		return answerId;
+	}
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+}
